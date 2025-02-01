3809cc613942b69a941aa666f70fe8da2b599e5f
alireza rahmani <150823639+alireza-rahmani991@users.noreply.github.com>
2025-02-01 22:29:58 +0330

    final touch

--- main.java
+++ main.java
@@ -2,9 +2,9 @@ import java.io.*;
 
 public class main {
     public static void main(String[] args){
-        String textFilePath = "text.txt";
-        String encodedFilePath = "encoded.hff";
-        String decodedFilePath = "decoded.txt";
+        String textFilePath = "inputAndOutputs/text.txt";
+        String encodedFilePath = "inputAndOutputs/encoded.hff";
+        String decodedFilePath = "inputAndOutputs/decoded.txt";
         
         // huffman code = new huffman(textFilePath, encodedFilePath );
         huffman code = new huffman(encodedFilePath, decodedFilePath );
